093de9b123cb3bdc2438af60d643a1e61628edaf
plata <eyoung.cho@gmail.com>
2019-07-09 20:02:19 +0200

    Added get hash function. Refactorized test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import hashlib
 import os
 from collections import defaultdict
 
@@ -5,32 +6,49 @@ from collections import defaultdict
 root_dir = "/Users/eunyoungcho/Pictures/2019/example"
 
 
+def hash_file(filepath):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
+   # make a hash object
+   hasher = hashlib.sha1()
+   # open file for reading in binary mode
+   with open(filepath, 'rb') as file:
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           hasher.update(chunk)
+   # return the hex representation of digest
+   return hasher.hexdigest()
+
 def get_file_list(root_dir):
-    size_by_file_dict = {}
+    hash_by_file_dict = {}
 
     for dirpath, subdirs, file_list in os.walk(root_dir):
         file_list = ignore_hidden_files(file_list)
-        for file in file_list:
-            file_size = os.path.getsize(root_dir + "/{}".format(file))
-            size_by_file_dict[root_dir+'/'+file] = file_size
+        for filename in file_list:
+            filepath = os.path.join(dirpath, filename)
+            file_hash = hash_file(filepath)
+            hash_by_file_dict[filepath] = file_hash
 
-    print("lista de archivos", size_by_file_dict)
+    print("lista de archivos", hash_by_file_dict)
 
-    return size_by_file_dict
+    return hash_by_file_dict
 
 
 def reverse_index_file_list(root_dir):
-    size_by_file_dict = get_file_list(root_dir)
+    hash_by_file_dict = get_file_list(root_dir)
 
-    file_by_size_dict = defaultdict(set)
+    file_by_hash_dict = defaultdict(set)
 
-    for file, size in size_by_file_dict.items():
-        set_ = file_by_size_dict[size]
+    for file, size in hash_by_file_dict.items():
+        set_ = file_by_hash_dict[size]
         set_.add(file)
 
-    print('reverse_index', file_by_size_dict)
+    print('reverse_index', file_by_hash_dict)
 
-    return file_by_size_dict
+    return file_by_hash_dict
 
 
 def ignore_hidden_files(file_list):
@@ -48,22 +66,3 @@ if __name__ == '__main__':
     get_file_list(root_dir)
     reverse_index_file_list(root_dir)
 
-#
-# import hashlib
-# def hash_file(filename):
-#    """"This function returns the SHA-1 hash
-#    of the file passed into it"""
-#    # make a hash object
-#    h = hashlib.sha1()
-#    # open file for reading in binary mode
-#    with open(filename,'rb') as file:
-#        # loop till the end of the file
-#        chunk = 0
-#        while chunk != b'':
-#            # read only 1024 bytes at a time
-#            chunk = file.read(1024)
-#            h.update(chunk)
-#    # return the hex representation of digest
-#    return h.hexdigest()
-# message = hash_file("track1.mp3")
-# print(message)
--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 import main
+import os
 
 
 class TestProject(TestCase):
@@ -28,7 +29,19 @@ class TestProject(TestCase):
         and its values are the file's names.
         """
         root_dir = "/Users/eunyoungcho/Pictures/2019/example"
-        result = main.reverse_index_file_list(root_dir)
+        ret = {
+            'IMG_0201.JPG': 1190906,
+            'IMG_0210.JPG': 1190906,
+            'IMG_0207.JPG': 1928182,
+            'IMG_0401.JPG': 1830585,
+            'IMG_0400.JPG': 1844840,
+            'IMG_0301.JPG': 1830585,
+            'IMG_0300.JPG': 1844840,
+            'IMG_0208.JPG': 2023441,
+            'IMG_0209.JPG': 2000484
+        }
+        with mock.patch("main.get_file_list", return_value=ret) as mocked_fn:
+            result = main.reverse_index_file_list(root_dir)
 
         expected_value = {
             1844840: {'IMG_0300.JPG', 'IMG_0400.JPG'},
@@ -40,6 +53,7 @@ class TestProject(TestCase):
 
         self.assertIsInstance(result, dict)
         self.assertEqual(result, expected_value)
+        mocked_fn.assert_called_once_with(root_dir)
 
     def test_ignore_hidden_files(self):
         """
@@ -52,3 +66,13 @@ class TestProject(TestCase):
 
         self.assertEqual(expected_value, result)
 
+    def test_sha1_hasher(self):
+        """
+        Should return the sha1 hash of a file
+        """
+        moduledirpath = os.path.dirname(__file__)
+        filepath = os.path.join(moduledirpath, "test_file.txt")
+        result = main.hash_file(filepath)
+
+        expected_value = "50a79820e404c67ebbeb3f79ad96596cf1716be2"
+        self.assertEqual(expected_value, result)
