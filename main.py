8a258e0cba2f173cae695c09e3f8ac0347228d9b
plata <eyoung.cho@gmail.com>
2019-07-23 20:43:41 +0200

    Añadidas las funciones que completan el comando y tests

--- main.py
+++ main.py
@@ -3,26 +3,24 @@ import os
 from collections import defaultdict
 
 
-root_dir = "/Users/eunyoungcho/Pictures/2019/example"
-
-
 def hash_file(filepath):
-   """"This function returns the SHA-1 hash
-   of the file passed into it"""
-   # make a hash object
-   hasher = hashlib.sha1()
-   # open file for reading in binary mode
-   with open(filepath, 'rb') as file:
-       # loop till the end of the file
-       chunk = 0
-       while chunk != b'':
-           # read only 1024 bytes at a time
-           chunk = file.read(1024)
-           hasher.update(chunk)
-   # return the hex representation of digest
-   return hasher.hexdigest()
+    """This function returns the SHA-1 hash
+    of the file passed into it"""
+    # make a hash object
+    hasher = hashlib.sha1()
+    # open file for reading in binary mode
+    with open(filepath, 'rb') as file:
+        # loop till the end of the file
+        chunk = 0
+        while chunk != b'':
+            # read only 1024 bytes at a time
+            chunk = file.read(1024)
+            hasher.update(chunk)
+    # return the hex representation of digest
+    return hasher.hexdigest()
 
-def get_file_list(root_dir):
+
+def get_hash_by_file(root_dir):
     hash_by_file_dict = {}
 
     for dirpath, subdirs, file_list in os.walk(root_dir):
@@ -32,23 +30,42 @@ def get_file_list(root_dir):
             file_hash = hash_file(filepath)
             hash_by_file_dict[filepath] = file_hash
 
-    print("lista de archivos", hash_by_file_dict)
-
     return hash_by_file_dict
 
 
-def reverse_index_file_list(root_dir):
-    hash_by_file_dict = get_file_list(root_dir)
+def get_reverse_index(map):
+    reverse_dict = defaultdict(set)
 
-    file_by_hash_dict = defaultdict(set)
+    for k, v in map.items():
+        set_ = reverse_dict[v]
+        set_.add(k)
 
-    for file, size in hash_by_file_dict.items():
-        set_ = file_by_hash_dict[size]
-        set_.add(file)
+    return reverse_dict
 
-    print('reverse_index', file_by_hash_dict)
 
-    return file_by_hash_dict
+def format_file_by_hash_dict(files_by_hash):
+    list_of_repeated_files = []
+    for files in files_by_hash.values():
+        if len(files) > 1:
+            list_of_repeated_files.append(list(files))
+
+    summary = []
+    for repeated_files in list_of_repeated_files:
+        reference = repeated_files[0]
+        rest = repeated_files[1:]
+        summary.append((reference, rest))
+
+    return summary
+
+
+def print_summary(summary):
+    print('I think you have repeated files:')
+
+    for reference, repeated in summary:
+        print(f'\t{reference} is repeated in:')
+        for repetition in repeated:
+            print(f'\t\t{repetition}')
+        print()
 
 
 def ignore_hidden_files(file_list):
@@ -61,8 +78,13 @@ def ignore_hidden_files(file_list):
     return result
 
 
+def command_find_repeated(root_dir):
+    hash_by_file = get_hash_by_file(root_dir)
+    files_by_hash = get_reverse_index(hash_by_file)
+    summary = format_file_by_hash_dict(files_by_hash)
+    print_summary(summary)
+
 
 if __name__ == '__main__':
-    get_file_list(root_dir)
-    reverse_index_file_list(root_dir)
-
+    root_dir = "/Users/eunyoungcho/Pictures/2019/example"
+    command_find_repeated(root_dir)
--- test_main.py
+++ test_main.py
@@ -10,14 +10,19 @@ class TestProject(TestCase):
         Should return a dictionary related by file and its size.
         """
         root_dir = "/Users/eunyoungcho/Pictures/2019/example"
-        result = main.get_file_list(root_dir)
+        result = main.get_hash_by_file(root_dir)
 
         expected_value = {
-            '/Users/eunyoungcho/Pictures/2019/example/IMG_0201.JPG': 1190906, '/Users/eunyoungcho/Pictures/2019/example/IMG_0210.JPG': 1190906,
-            '/Users/eunyoungcho/Pictures/2019/example/IMG_0207.JPG': 1928182, '/Users/eunyoungcho/Pictures/2019/example/IMG_0401.JPG': 1830585,
-            '/Users/eunyoungcho/Pictures/2019/example/IMG_0400.JPG': 1844840, '/Users/eunyoungcho/Pictures/2019/example/IMG_0301.JPG': 1830585,
-            '/Users/eunyoungcho/Pictures/2019/example/IMG_0300.JPG': 1844840, '/Users/eunyoungcho/Pictures/2019/example/IMG_0208.JPG': 2023441,
-            '/Users/eunyoungcho/Pictures/2019/example/IMG_0209.JPG': 2000484, '/Users/eunyoungcho/Pictures/2019/example/a/IMG_0207.JPG': 1928182
+            '/Users/eunyoungcho/Pictures/2019/example/IMG_0201.JPG': '1587fd830af0e02c6a0f6c0d4f21f2af4827989b',
+            '/Users/eunyoungcho/Pictures/2019/example/IMG_0210.JPG': '1587fd830af0e02c6a0f6c0d4f21f2af4827989b',
+            '/Users/eunyoungcho/Pictures/2019/example/IMG_0207.JPG': '39332041f67ef96a37fa4dc0ba4e97d4d3a97ce3',
+            '/Users/eunyoungcho/Pictures/2019/example/IMG_0401.JPG': '87a4cfcfebde26b547695204c95b75ea101e25ea',
+            '/Users/eunyoungcho/Pictures/2019/example/IMG_0400.JPG': 'ce1b35c4cb181fcbd7968f1706207254714e6eb1',
+            '/Users/eunyoungcho/Pictures/2019/example/IMG_0301.JPG': '87a4cfcfebde26b547695204c95b75ea101e25ea',
+            '/Users/eunyoungcho/Pictures/2019/example/IMG_0300.JPG': 'ce1b35c4cb181fcbd7968f1706207254714e6eb1',
+            '/Users/eunyoungcho/Pictures/2019/example/IMG_0208.JPG': '2990eaa4cbbfeb5d8c1d5d7b2da73f118a247652',
+            '/Users/eunyoungcho/Pictures/2019/example/IMG_0209.JPG': '3f647bbc7bb1911a41ace5291772fccc422e13df',
+            '/Users/eunyoungcho/Pictures/2019/example/a/IMG_0207.JPG': '39332041f67ef96a37fa4dc0ba4e97d4d3a97ce3'
         }
 
         self.assertIsInstance(result, dict)
@@ -28,32 +33,50 @@ class TestProject(TestCase):
         Should return a dictionary where the key is the files's size
         and its values are the file's names.
         """
-        root_dir = "/Users/eunyoungcho/Pictures/2019/example"
-        ret = {
-            'IMG_0201.JPG': 1190906,
-            'IMG_0210.JPG': 1190906,
-            'IMG_0207.JPG': 1928182,
-            'IMG_0401.JPG': 1830585,
-            'IMG_0400.JPG': 1844840,
-            'IMG_0301.JPG': 1830585,
-            'IMG_0300.JPG': 1844840,
-            'IMG_0208.JPG': 2023441,
-            'IMG_0209.JPG': 2000484
+        map = {
+            'IMG_0201.JPG': '234s',
+            'IMG_0210.JPG': '234s',
+            'IMG_0207.JPG': 'hy345',
+            'IMG_0401.JPG': '75rt',
+            'IMG_0400.JPG': '1234',
+            'IMG_0301.JPG': '75rt',
+            'IMG_0300.JPG': '1234',
+            'IMG_0208.JPG': '3ge43',
+            'IMG_0209.JPG': 'c2g4'
         }
-        with mock.patch("main.get_file_list", return_value=ret) as mocked_fn:
-            result = main.reverse_index_file_list(root_dir)
+        result = main.get_reverse_index(map)
 
         expected_value = {
-            1844840: {'IMG_0300.JPG', 'IMG_0400.JPG'},
-            1830585: {'IMG_0401.JPG', 'IMG_0301.JPG'},
-            1190906: {'IMG_0201.JPG', 'IMG_0210.JPG'},
-            1928182: {'IMG_0207.JPG'}, 2023441: {'IMG_0208.JPG'},
-            2000484: {'IMG_0209.JPG'}
+            '1234': {'IMG_0300.JPG', 'IMG_0400.JPG'},
+            '75rt': {'IMG_0401.JPG', 'IMG_0301.JPG'},
+            '234s': {'IMG_0201.JPG', 'IMG_0210.JPG'},
+            'hy345': {'IMG_0207.JPG'},
+            '3ge43': {'IMG_0208.JPG'},
+            'c2g4': {'IMG_0209.JPG'}
         }
 
         self.assertIsInstance(result, dict)
         self.assertEqual(result, expected_value)
-        mocked_fn.assert_called_once_with(root_dir)
+
+    def test_format_file_by_hash_dict(self):
+        map = {
+            '1234': ('IMG_0300.JPG', 'IMG_0400.JPG'),
+            '75rt': ('IMG_0401.JPG', 'IMG_0301.JPG'),
+            '234s': ('IMG_0201.JPG', 'IMG_0210.JPG'),
+            'hy345': ('IMG_0207.JPG',),
+            '3ge43': ('IMG_0208.JPG',),
+            'c2g4': ('IMG_0209.JPG',)
+        }
+
+        expected_value = [
+            ('IMG_0300.JPG', ['IMG_0400.JPG']),
+            ('IMG_0401.JPG', ['IMG_0301.JPG']),
+            ('IMG_0201.JPG', ['IMG_0210.JPG'])
+        ]
+
+        result = main.format_file_by_hash_dict(map)
+        self.assertIsInstance(result, list)
+        self.assertEqual(result, expected_value)
 
     def test_ignore_hidden_files(self):
         """
